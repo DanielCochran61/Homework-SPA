9e6a4d9ecc484787b72029216b2fd501a8e9152b
Daniel Cochran <daniel.cochran61@gmail.com>
2019-02-28 11:24:43 -0500

    update all files

--- app.js
+++ app.js
@@ -1,67 +1,126 @@
+const render = function () {
+    $('.content').empty();
+
+    for (let i = 0; i < employeeList.length; i++) {
+        let nameIn = employeeList[i].name;
+        let officeNum = employeeList[i].officeNum;
+        let phoneNum = employeeList[i].phoneNum;
+
+        $('.content').append(`<p>${nameIn}<br>${officeNum}<br>${phoneNum}</p>`);
+    }
+}
+render();
+
+const addEmployee = function () {
+    const nameVal = $('#nameInput').val();
+    const officeVal = $('#officeInput').val();
+    const phoneVal = $('#phoneInput').val();
+
+    const nameList = [];
+    for (let i = 0; i < employeeList.length; i++) {
+        nameList.push(employeeList[i].name);
+    }
+    if (!nameList.includes(nameVal)) {
+        employeeList.push({
+            name: nameVal,
+            officeNum: officeVal,
+            phoneNum: phoneVal
+        });
+    }
+    $('#nameInput').val('');
+    $('#officeInput').val('');
+    $('#phoneInput').val('');
+    render();
+}
+$('#addNew').on('click', addEmployee);
+
+const verifyName = function () {
+    const nameVal = $('#nameSearch').val();
+
+    const nameList = [];
+    for (let i = 0; i < employeeList.length; i++) {
+        nameList.push(employeeList[i].name);
+    }
+
+    if (nameList.includes(nameVal)) {
+        $('#result').text('Yes');
+    } else {
+        $('#result').text('No');
+    }
+}
+$('#search').on('click', verifyName);
+
+const updateData = function () {
+    const nameVal = $('#nametoUpdate').val();
+    const officeVal = $('#officeUpdate').val();
+    const phoneVal = $('#phoneUpdate').val();
+
+    for (let i = 0; i < employeeList.length; i++) {
+        if (employeeList[i].name === nameVal) {
+            employeeList.splice(i, 1, {
+                name: nameVal,
+                officeNum: officeVal,
+                phoneNum: phoneVal
+            });
+        }
+    }
+    $('#nametoUpdate').val('');
+    $('#officeUpdate').val('');
+    $('#phoneUpdate').val('');
+    render();
+}
+$('#updateEmp').on('click', updateData);
+
+const deleteData = function () {
+    const nameVal = $('#nametoDelete').val();
+
+    for (let i = 0; i < employeeList.length; i++) {
+        if (employeeList[i].name === nameVal) {
+            employeeList.splice(i, 1);
+        }
+    }
+    $('#nametoDelete').val('');
+    render();
+}
+$('#deleteEmp').on('click', deleteData);
+
 (function () {
-    $('#addPage').hide();
-    $('#verifyPage').hide();
-    $('#updatePage').hide();
-    $('#deletePage').hide();
-    $('#viewPage').show();
-})()
+    $('#addpage').hide();
+    $('#verifypage').hide();
+    $('#updatepage').hide();
+    $('#deletepage').hide();
+})();
 
 const showView = function () {
-    $('#addPage').hide();
-    $('#verifyPage').hide();
-    $('#updatePage').hide();
-    $('#deletePage').hide();
-    $('#viewPage').show();
-
-    render(employeeList)
-};
-$('#view').on('click', showView);
-
+    $('section').hide();
+    $('#viewpage').show();
+}
 
 const showAdd = function () {
-    $('#viewPage').hide();
-    $('#verifyPage').hide();
-    $('#updatePage').hide();
-    $('#deletePage').hide();
-    $('#addPage').show();
-};
-
-$('#add').on('click', showAdd);
-
+    $('section').hide();
+    $('#addpage').show();
+}
 
 const showVerify = function () {
-    $('#addPage').hide();
-    $('#viewPage').hide();
-    $('#updatePage').hide();
-    $('#deletePage').hide();
-    $('#verifyPage').show();
-};
+    $('#nameSearch').val('');
+    $('#result').empty();
 
-const verifyFunc = function () {
-    if (true) {return yes} 
-    else {return no} 
-}; 
-
-$('verify').on('click', showVerify);
-
+    $('section').hide();
+    $('#verifypage').show();
+}
 
 const showUpdate = function () {
-    $('#addPage').hide();
-    $('#verifyPage').hide();
-    $('#viewPage').hide();
-    $('#deletePage').hide();
-    $('#updatePage').show();
-};
-
-$('update').on('click', showUpdate);
-
+    $('section').hide();
+    $('#updatepage').show();
+}
 
 const showDelete = function () {
-    $('#addPage').hide();
-    $('#verifyPage').hide();
-    $('#updatePage').hide();
-    $('#viewPage').hide();
-    $('#deletePage').show();
-};
+    $('section').hide();
+    $('#deletepage').show();
+}
 
-$('delete').on('click', showDelete);
+$('#view').on('click', showView);
+$('#add').on('click', showAdd);
+$('#verify').on('click', showVerify);
+$('#update').on('click', showUpdate);
+$('#delete').on('click', showDelete);
